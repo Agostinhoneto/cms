1d1511d3c9a867921dad634d3807bd3d53024624
agostinho f s neto silva netto <agostneto6@gmail.com>
2023-08-28 22:45:30 -0300

    criando pastas

--- index.php
+++ index.php
@@ -1,12 +1,34 @@
 <?php
-$host = 'mysql:host=localhost;dbname=cms';
-$username = 'root';
-$password = '';
-
-try {
-    $conn = new PDO($host, $username, $password);   
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erro na conexão: " . $e->getMessage());
+session_start();
+if (!isset($_SESSION['username'])) {
+    header('Location: login.php');
+    exit;
 }
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Meu Site Institucional</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="quem-somos.html">Quem Somos</a></li>
+                <li><a href="contato.html">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <section id="conteudo">
+        <!-- Conteúdo da página -->
+    </section>
+    
+    <footer>
+        <p>&copy; 2023 Meu Site Institucional</p>
+    </footer>
+</body>
+</html>
